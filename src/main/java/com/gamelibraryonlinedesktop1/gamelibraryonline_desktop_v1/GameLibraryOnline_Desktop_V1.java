38e6efc1c3578ab235a7ae5210ccf39cffc825c5
Dimitri MOMPEROUSSE <momperousse.dimitri.pro@gmail.com>
2025-05-04 06:41:29 +0200

    First Commit

---
+++ src/main/java/com/gamelibraryonlinedesktop1/gamelibraryonline_desktop_v1/GameLibraryOnline_Desktop_V1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gamelibraryonlinedesktop1.gamelibraryonline_desktop_v1;
+
+/**
+ *
+ * @author dimit
+ */
+public class GameLibraryOnline_Desktop_V1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
